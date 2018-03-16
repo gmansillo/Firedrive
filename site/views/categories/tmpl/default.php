3fe57c33d6416ba0834248b8fcd1a77815015fc4
Giovanni Mansillo <giovanni.mansillo@gmail.com>
2018-03-16 13:04:10 +0100

    Added list and listitem role attributes for frontens lists as required by ARIA accessibility rules

--- site/views/categories/tmpl/default.php
+++ site/views/categories/tmpl/default.php
@@ -31,7 +31,7 @@ jQuery(function($) {
 	});
 });");
 ?>
-<div class="categories-list<?php echo $this->pageclass_sfx; ?>">
+<div class="categories-list<?php echo $this->pageclass_sfx; ?>" role="list">
     <?php
     echo JLayoutHelper::render('joomla.content.categories_default', $this);
     echo $this->loadTemplate('items');
